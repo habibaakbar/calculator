6197a10eae538c66e19f8bb7d2304b6e3f9f7406
Habiba <habibaakbar062@gmail.com>
2024-02-01 11:57:06 +0500

    add new code

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-console.log('hiba');
-// alert('hiba');
-
-let string = "";
-let buttons = document.querySelector('.button');
-Array.from(buttons).forEach((button) => {
-    button.addEeventlistener('click', (e)=>{
-        if (e.target.innerHTML ==  '=') {
+let string = ""
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click' , (e)=>{
+        if (e.target.innerHTML == '=') {
             string = eval(string);
             document.querySelector('input').value = string;
-
         }
-        else if(e.target.innerHTML ==  'C') {
+       else if (e.target.innerHTML == 'C') {
             string = ""
             document.querySelector('input').value = string;
-
         }
-         
-       
+
         else{
-            console.log('e.target')
+
+            console.log(e.target)
             string = string + e.target.innerHTML;
             document.querySelector('input').value = string;
         }
+        
     })
-});
+})
